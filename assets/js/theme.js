a2e4b3856e2932b0b3152ace16af0ad76d9f652f
Mohamed El Amine Boudjoghra <95423980+aminebdj@users.noreply.github.com>
2023-06-23 12:54:16 +0400

    updated

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -46,16 +46,16 @@ function setTheme(value) {
 }
 
 var coll = document.getElementsByClassName("collapsible");
-var i;
+  var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      id = this.id;
+      var content = document.getElementById(id.replace("collapse", "content"));
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
